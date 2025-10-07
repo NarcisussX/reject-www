da28f6868c945fa5e6c18649cf4d89cd73fe3620
NarcisussX <cooperbroderick25@gmail.com>
2025-10-06 22:31:21 -0500

    REBRAND BABY

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
+// src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
-import "./styles.css";
+import Home from "./pages/Home";
+import JCode from "./pages/JCode";
+import Admin from "./pages/Admin";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "j/:jcode", element: <JCode /> },
+      { path: "admin", element: <Admin /> },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
